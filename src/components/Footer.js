e40e1b26ebad292dd4c00a6f934979bd98d381e3
murat26 <74151892+muratyayla26@users.noreply.github.com>
2021-01-10 11:43:41 +0300

    href updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ const Footer = () => {
     return (
         <footer className="info">
             <p>Click to edit a todo</p>
-            <p>Created by <a href="">Dmitry Sharabin</a></p>
-            <p>Part of <a href="">TodoMVC</a></p>
+            <p>Created by <a href="#">Dmitry Sharabin</a></p>
+            <p>Part of <a href="#">TodoMVC</a></p>
         </footer>
     );
 };
